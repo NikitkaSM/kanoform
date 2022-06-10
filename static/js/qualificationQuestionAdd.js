1c9f9cf878f3904904a42b2404d452d883b108fb
NikitkaSM <nikitkasmo@gmail.com>
2022-06-10 17:53:42 +0300

    answer variants

--- static/js/qualificationQuestionAdd.js
+++ static/js/qualificationQuestionAdd.js
@@ -1,21 +1,37 @@
-const qualificationQuestionButton = document.getElementById(
-  'qualificationQuestionButton'
-);
-const qualificationQuestionsContainer = document.getElementById(
-  'qualificationQuestionsContainer'
-);
+const qualificationQuestionButton = document.getElementById('qualificationQuestionButton');
+const qualificationQuestionsContainer = document.getElementById('qualificationQuestionsContainer');
 let qualificationQuestionCounter = 1;
+let answerVariantCounter = 0;
+
+const addAnswerVariant = event => {
+  const button = event.target;
+  const questionContainer = button.parentNode;
+  const answerVariantsContainer = questionContainer.querySelector('ul');
+  const answerVariant = document.createElement('li');
+  const answerVariants = document.getElementsByClassName('qualificationAnswerVariant');
+
+  if (answerVariants.length === 2) {
+    alert('Должно быть минимум два варианта ответа');
+    return;
+  }
+
+  answerVariant.setAttribute('class', 'qualificationAnswerVariant');
+  
+  answerVariantsContainer.append(answerVariant);
+
+
+};
 
 const deleteQualificationQuestion = event => {
   const questions = document.getElementsByClassName('qualificationQuestion');
 
   if (questions.length === 1) {
-    alert("Должен быть как минимум один вопрос");
+    alert('Должен быть как минимум один вопрос');
     return;
   }
 
-  let button = event.target;
-  let li = button.parentNode;
+  const button = event.target;
+  const li = button.parentNode;
 
   li.remove();
   qualificationQuestionCounter -= 1;
@@ -30,10 +46,7 @@ const deleteQualificationQuestion = event => {
   for (let x = 0; x < qualificationQuestionCounter - 1; x++) {
     const questions_element_added = Object.entries(questions)[x][1];
 
-    questions_element_added.setAttribute(
-      'id',
-      `qualification-${second_counter}`
-    );
+    questions_element_added.setAttribute('id', `qualification-${second_counter}`);
     second_counter++;
   }
 };
@@ -45,6 +58,8 @@ const addQualificationQuestion = event => {
     alert('Максимальное кол-во вопросов');
     return;
   }
+
+  const answerVariantAddButton = document.createElement('button');
   const li = document.createElement('li');
   const input = document.createElement('input');
   const deleteButton = document.createElement('button');
@@ -57,26 +72,27 @@ const addQualificationQuestion = event => {
   input.setAttribute('type', 'text');
   input.setAttribute('class', 'container form rounded question-input');
   input.setAttribute('id', `featureInput${qualificationQuestionCounter}`);
-  input.setAttribute("placeholder", "Введите вопрос");
+  input.setAttribute('placeholder', 'Введите вопрос');
 
-  deleteButton.setAttribute("type", "button");
+  deleteButton.setAttribute('type', 'button');
   deleteButton.addEventListener('click', deleteQualificationQuestion);
 
-  const qualificationQuestionContainer = document.getElementById(
-    `qualification-${qualificationQuestionCounter}`
-  );
+  answerVariantAddButton.setAttribute('class', 'answer-variant-add-button btn');
+  answerVariantAddButton.setAttribute('type', 'button');
+  answerVariantAddButton.innerText = 'Добавить вариант ответа';
+  answerVariantAddButton.addEventListener('click', addAnswerVariant);
+
+  const qualificationQuestionContainer = document.getElementById(`qualification-${qualificationQuestionCounter}`);
 
   qualificationQuestionContainer.append(input);
   qualificationQuestionContainer.append(deleteButton);
+  qualificationQuestionContainer.append(answerVariantAddButton);
+  qualificationQuestionContainer.append(document.createElement('ul'));
 
-  deleteButton.setAttribute(
-    'class',
-    'rounded btn delete-button qualification-question'
-  );
+  deleteButton.setAttribute('class', 'rounded btn delete-button qualification-question');
   deleteButton.innerText = 'delete';
 
   qualificationQuestionCounter++;
-
 };
 
 qualificationQuestionButton.addEventListener('click', addQualificationQuestion);
